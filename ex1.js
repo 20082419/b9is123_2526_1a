1163d7d316801b2dda840b84fdec88069ec52136
20082419 <20082419@mydbs.ie>
2025-09-30 15:41:48 +0100

    chore: update check function

--- ex1.js
+++ ex1.js
@@ -1,3 +1,24 @@
+//alert("Hello");//write a check function here...
+//update: the check function should reveal the "removed" text input
+//if the baggage is overweight
+//and a button to remove some
+//the remove function should update the weight
+//and let the user know when ok to proceed
+
+let check = () => alert('Checking Baggage weight: ' + parseInt(document.getElementById('weight').value));
+let remove = () => alert('Removing weight: ' + parseInt(document.getElementById('removed').value))
+
 //write a function to alert if overweight (15kg) baggage
+let weight = parseInt(document.getElementById('weight').value);
+
+const checkBaggageWeight = () => {
+  if (weight > 15) {
+    alert("Overweight baggage detected!");
+    document.getElementById("removed").style.display = "block";
+    document.getElementById("remB").style.display = "block";
+  } else {
+    return check();
+  }
+}
 
 //call it from the page
