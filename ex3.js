c7c056051fb025a08eb04b9764f400a80e5d1b6a
20082419 <20082419@mydbs.ie>
2025-10-20 01:10:26 +0100

    ex3 draft

--- ex3.js
+++ ex3.js
@@ -1,24 +1,33 @@
-let listEuler1=(a,b,l)=>0;//obviously wrong - compute the sum of multiples of a or b in list l
-let listEuler2=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
-let listEuler3=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
+let a = parseInt(document.getElementById("a1").value);
+let b = parseInt(document.getElementById("b1").value);
+let l = document.getElementById("l").value;
 
-let eulerlist=()=>
-{
-    a=2//can un-hardcode
-    b=3
-    l=[1,2,3,4,5,6,7,9,10,10,10]  //document.getElementById('l').value.split(" ").map((x=>parseInt(x)); // if l is space separated numbers
-    //call listEuler1 and alert. - compute the sum of multiples of a or b in list l
-}
-let euler2Lists=()=>
-{
-    a=[2,3]//can un-hardcode
-    l=[1,2,3,4,5,6,7,9,10,10,10]
+let listArray = [];
+
+const eulerlist = () => {
+    for (let i = 0; i < l.length; i++) {
+        listArray.push(parseInt(i));
+    }
+
+    let sum = 0;
+
+    for (let i = 0; i <= listArray.length - 1; i++) {
+        if (i % a === 0 || i % b === 0) {
+            sum += listArray[i];
+        }
+    }
+
+    return alert(sum);
+};
+
+let euler2Lists = () => {
+    a = [2, 3]; //can un-hardcode
+    l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
     //call listEuler2 and alert.
-}
+};
 
-let euler2Lists1=()=>
-{
-    a=[2,3,5]//can un-hardcode
-    l=[1,2,3,4,5,6,7,9,10,10,10]
+let euler2Lists1 = () => {
+    a = [2, 3, 5]; //can un-hardcode
+    l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
     //call listEuler3 and alert.
-}
+};
